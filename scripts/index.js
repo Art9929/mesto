300b7405515a709ae1ddf8f94e328fe9211d68e1
Artem <loginryabtsev>
2023-01-19 17:33:24 +0300

    bagfix_1

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,13 @@
-let popupBtnOpen = document.querySelector(".info__edit-button");
+let popupBtnOpen = document.querySelector(".profile__edit-button");
 let popupWindow =   document.querySelector(".popup");
 let popupBtnClose = document.querySelector(".popup__close");
-let titleAutor =  document.querySelector(".info__title");
-let subtitleAutor =  document.querySelector(".info__subtitle");
+let titleAutor =  document.querySelector(".profile__title");
+let subtitleAutor =  document.querySelector(".profile__subtitle");
 // Находим форму в DOM
 // Находим поля формы в DOM
 let formElement = document.querySelector(".popup__form");
 let nameInput = document.querySelector(".popup__input_text_name");
 let jobInput = document.querySelector(".popup__input_text_job");
-
-popupBtnOpen.addEventListener("click", openPopup);
-popupBtnClose.addEventListener("click", closePopup);
 
 function openPopup() {
   popupWindow.classList.add("popup_opened");
@@ -30,9 +27,7 @@ function handleFormSubmit (evt) {
                           // Так мы можем определить свою логику отправки.
                           // О том, как это делать, расскажем позже.
 
-// Получите значение полей jobInput и nameInput из свойства value
-  nameInput.value;
-  jobInput.value;
+                          // Получите значение полей jobInput и nameInput из свойства value
 // Вставьте новые значения с помощью textContent
   titleAutor.textContent = nameInput.value;
   subtitleAutor.textContent = jobInput.value;
@@ -42,3 +37,7 @@ function handleFormSubmit (evt) {
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
+
+// Listener
+popupBtnOpen.addEventListener("click", openPopup);
+popupBtnClose.addEventListener("click", closePopup);
