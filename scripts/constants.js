ecd9ab2f75b24cfc9e502738aae1d3cd71475bca
Artem <loginryabtsev>
2023-02-03 13:07:05 +0300

    project_5_6

---
+++ scripts/constants.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Карачаевск",
+    link: "./images/karachevsk.jpg",
+  },
+  {
+    name: "Гора Эльбрус",
+    link: "./images/elbrus.jpg",
+  },
+  {
+    name: "Домбай",
+    link: "./images/dombai.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "./images/chelyabinsk_oblast.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "./images/kamchatka.jpg",
+  },
+  {
+    name: "Карачаево-Черкесия",
+    link: "./images/karachevsk.jpg",
+  },
+];
